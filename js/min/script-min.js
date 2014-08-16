2405d237e8c25d55f39e9f15ec3fd474c06e7df2
Sasha <oleksandr.lysyi@gmail.com>
2014-08-16 10:06:56 +0300

    addd fooooter

--- js/min/script-min.js
+++ js/min/script-min.js
@@ -1,1 +1,1 @@
-$(".eject-button").click(function(t){t.preventDefault(),$(".multimedia-block").css("height")>"1px"?$(".multimedia-block").animate({height:"0px"},500):$(".multimedia-block").animate({height:"70px"},500)});
+$(".eject-button").click(function(t){t.preventDefault(),$(".multimedia-block").css("height")>"1px"?$(".multimedia-block").animate({height:"0px"},500):$(".multimedia-block").animate({height:"90px"},500)});
